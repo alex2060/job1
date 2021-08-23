f6764c174fcde26deaf8d2c72c6b52a7ac868f72
alex2060 <alex.haussmann@gmail.com>
2021-08-22 18:05:24 -0600

    wearedone

--- pyspark/django_form_other_project/mysite/numb/url.py
+++ pyspark/django_form_other_project/mysite/numb/url.py
@@ -3,11 +3,14 @@ from . import views
 
 
 urlpatterns = [
-	path('add_user', views.home,name='home'),
-    path('add_traid', views.home,name='add_traid'),
-    path('post_reqest', views.post,name='post'),
-    path('lbry_host', views.post,name='post'),
-    path('cash', views.post,name='get_from_cash')
-]
+	path('traider', views.traider,name='traider'),
+    path('add_traid', views.add_traid,name='add_traid'),
+    path('compleat_traid', views.compleat_traid,name='compleat_traid'),
+    path('get_user_info', views.print_convertion,name='get_user_info'),
+    path('convertion', views.print_convertion,name='convertion'),
+    path('print_user', views.print_user,name='print_user'),
+    path('doit', views.doit,name='doit')
 
+]
+#print_user
 
--- pyspark/django_form_other_project/mysite/numb/views.py
+++ pyspark/django_form_other_project/mysite/numb/views.py
@@ -3,44 +3,56 @@ from django.http import HttpResponse
 import time
 from django.core.files import File
 # Create your views here.
-import number_to_english as conv
+
 
 import lets_convert
 from django.shortcuts import render
+import mysql_test
 
-def home(req):
+
+def traider(req):
     
-    f = open("to_be_frontend.html", "r")
+    f = open("to_be_frontend_check_make_traid.html", "r")
     output= f.read()
     f.close()
     return HttpResponse( output )
 
 def add_traid(req):
-    f = open("to_be_frontend.html", "r")
+    f = open("add_user.html", "r")
     output= f.read()
     f.close()
     return HttpResponse( output )
 
 def compleat_traid(req):
-    f = open("to_be_frontend.html", "r")
+    f = open("to_be_frontend_check_fin_traid.html", "r")
     output= f.read()
     f.close()
     return HttpResponse( output )
 
 def print_convertion(req):
-    f = open("to_be_frontend.html", "r")
+    f = open("to_be_frontend_check_transaction.html.html", "r")
     output= f.read()
     f.close()
     return HttpResponse( output )
 
 def print_user(req):
-    f = open("to_be_frontend.html", "r")
+    f = open("to_be_frontend_check_user.html", "r")
     output= f.read()
     f.close()
     return HttpResponse( output )
 
 
-def doit():
+def pyspark(req):
+    return HttpResponse( "output6" )
+
+def reset(req):
+    return HttpResponse( "output6" )
+
+
+def doit(req):
+    print("in here")
+    #return HttpResponse( "mysting" )
+
     action_type=""
     try:
         action_type=req.GET["action_type"]
@@ -52,6 +64,18 @@ def doit():
         user=req.GET["user"]
     except:
         user=""
+
+    email=""
+    try:
+        email=req.GET["email"]
+    except:
+        email=""
+
+    phone=""
+    try:
+        phone=req.GET["phone"]
+    except:
+        phone=""
 
     password=""
     try:
@@ -68,9 +92,10 @@ def doit():
 
     request_amound=""
     try:
-        request_amound=req.GET["request_amound"]
+        request_amound=float(req.GET["request_amound"])
     except:
         pass
+
 
     request_type=""
     try:
@@ -84,48 +109,120 @@ def doit():
         send_type=req.GET["send_type"]
     except:
         pass
-
     send_amount=""
     try:
-        send_amount=req.GET["send_amount"]
+        send_amount=float(req.GET["send_amount"])
     except:
         pass
+    #127.0.0.1:8000/doit?action_type=adduser&user=v1&email=a&password=1&phone=1
+    #127.0.0.1:8000/doit?action_type=adduser&user=v3&email=a&password=1&phone=1
 
 
+    if action_type=="adduser":
+        if password!="":
+            pass
+        else:
+
+            return HttpResponse( "blank1" )
+        if user!="":
+            pass
+        else:
+            return HttpResponse( "blank2" )
+        if email!="":
+            pass
+        else:
+            return HttpResponse( "blank3" )
+        if phone!="":
+            pass
+        else:
+            return HttpResponse( "blank4" )
+        out=mysql_test.makeuseremail(user,email,password)
+        return HttpResponse( out )
+
+    #127.0.0.1:8000/doit?action_type=maketraid&user=v1&password=1&request_type=money1&send_type=money2&request_amound=1&send_amount=1
 
 
+    #SELECT * from `traidtable` WHERE `traid_id` LIKE 'mvqtpftuhlmhcyfneazdyysmouaajaobhaoxesqycbrrryjbbnwjnvhkopzzhaya';
+    if action_type=="maketraid":
+        if password!="":
+            pass
+        else:
+            return HttpResponse( "blank1" )
+        if user!="":
+            pass
+        else:
+            return HttpResponse( "blank2" )
+
+        if request_type not in ["money1","money2"]:
+
+            return HttpResponse( "blank4",request_type," done " )
+
+        if send_type not in ["money1","money2"]:
+            return HttpResponse( "blank5" )
+
+        if send_type==request_type:
+            return HttpResponse( "blank6" )
+
+        if request_amound=="":
+            return HttpResponse( "blank7" )
+        if send_amount=="":
+            return HttpResponse( "blank8" )
+        out=mysql_test.funtion_make_traid(user,password,request_type,request_amound,send_type,send_amount)
+        return HttpResponse( out )
+
+    #127.0.0.1:8000/doit?action_type=fintraid&user=v2&password=1&traid_id=vyyyihrlgoefyiznjngvnpgwaeduqqgpottlkgjrawvfeooinulyxwhgcezyhuej
+
+    if action_type=="fintraid":
+        if password!="":
+            pass
+        else:
+            return HttpResponse( "blank1" )
+        if user!="":
+            pass
+        else:
+            return HttpResponse( "blank2" )
+
+        if traid_id =="":
+            return HttpResponse( "blank4" )
 
 
-def post(req):
-    from selenium import webdriver
-    value="now value"
-    url = "https://odysee.com/$/embed/testing/dc8d2599724f8593a7c92dde815527dcf33c1b78?r=CnRc5Ve1u4xmW4mLp6hz2mxZg5QPQwiC"
-    driver = webdriver.Firefox()
-    driver.get(url)
-    driver.implicitly_wait(20)  # seconds
+        out=mysql_test.compleat_traid(user,password,traid_id)
+        mysql_test.log_traid(out)
 
-    embed = driver.find_elements_by_class_name("markdown-preview")
+        return HttpResponse( out )
 
-    value=embed[0].find_element_by_tag_name("p").text
-    print(value)
-    time.sleep(10)
-    f = open('/Users/ahauss/Desktop/lbry/files/mysight/mysite/cash.txt', 'w')
-    testfile = File(f)
-    testfile.write(str(value)+"done")
-    testfile.close()
-    f.close()
-    time.sleep(1)
+    #127.0.0.1:8000/doit?action_type=print_convertion
+ 
+
+    if action_type=="print_convertion":
+        return HttpResponse( mysql_test.print_convertions("</br>") )
+    #127.0.0.1:8000/doit?action_type=reset_convertion
 
 
+    if action_type=="reset_convertion":
+        mysql_test.reset_convertion()
+        return HttpResponse( "done" )
 
-    return HttpResponse("cashed" )
+    #127.0.0.1:8000/doit?action_type=Uprint&user=v2
+
+
+    if action_type=="Uprint":
+        return HttpResponse(mysql_test.user_acount(user,"</br>"))
+
+    
+
+
+    mysting=action_type+","+user+","+password+","+traid_id+","+request_amound+","+request_type+","+send_type+","+send_amount
+
+    return HttpResponse( mysting )
+
 
 def get_from_cash(req):
 
     return render_to_response('./test.html')
 
 
-    
+
     
 
 
